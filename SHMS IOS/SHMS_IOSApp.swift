605515ece97459c65d03dd358c3c9eaba0e6bf3a
MuhammadHamzaAhmed <hamza1575ahmed@gmail.com>
2022-05-09 00:03:32 +0500

    Initial Commit

---
+++ SHMS IOS/SHMS_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SHMS_IOSApp.swift
+//  SHMS IOS
+//
+//  Created by Muhammad Hamza on 09/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SHMS_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SHMS IOSUITests/SHMS_IOSUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SHMS_IOSUITestsLaunchTests.swift
+//  SHMS IOSUITests
+//
+//  Created by Muhammad Hamza on 09/05/2022.
+//
+
+import XCTest
+
+class SHMS_IOSUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
